34b6eb7fddedaa45e10a7cfb5e94183c02ce64d9
Nelson Cortes <nelsondmondragon@gmail.com>
2021-07-10 17:11:15 -0500

    Utilizando Servlet

---
+++ src/com/platzi/platziweb/servlet/ServletLogin.java
@@ -0,0 +1,56 @@
+package com.platzi.platziweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet(description = "Servlet mange request login", urlPatterns = { "/ServletLogin" })
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String user = request.getParameter("user");
+		String company = request.getParameter("company");
+		PrintWriter writer = response.getWriter();
+		if(user != null && company != null) {
+			if (company.equals("Platzi")) {
+				writer.println("Bienvenido a Platzi");
+			}else {
+				writer.println("Bienvenido...");
+			}
+		}else {
+			writer.println("Usuario Incorrecto");
+		}
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
